d7088f151b18af376a5eda0150a1af9972b6bd9b
poopoomarvin <hchs010077@gmail.com>
2016-07-15 21:43:14 +0800

    Game is almost done, checkingEndOfGame have to be done

--- BlokusMar/main.cpp
+++ BlokusMar/main.cpp
@@ -2,62 +2,270 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <fstream>
 #include "shapes.h"
 
 using namespace std;
-vector<block> blocks;
+vector<shape> shapes, shapesA, shapesB;
 
-void init()
+char board[14][14];
+char tmpBoard[14][14];
+int shoulder[4][2] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+int side[4][2] = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+ifstream ifs ("CurrentBoard.txt", ifstream::in);
+ofstream ofs ("CurrentBoard.txt", ofstream::out);
+void listShapes(char player)
 {
-    blocks.push_back(assignBlock_00());blocks.push_back(assignBlock_01());blocks.push_back(assignBlock_02());blocks.push_back(assignBlock_03());blocks.push_back(assignBlock_04());
-    blocks.push_back(assignBlock_05());blocks.push_back(assignBlock_06());blocks.push_back(assignBlock_07());blocks.push_back(assignBlock_08());blocks.push_back(assignBlock_09());
-    blocks.push_back(assignBlock_10());blocks.push_back(assignBlock_11());blocks.push_back(assignBlock_12());blocks.push_back(assignBlock_13());blocks.push_back(assignBlock_14());
-    blocks.push_back(assignBlock_15());blocks.push_back(assignBlock_16());blocks.push_back(assignBlock_17());blocks.push_back(assignBlock_18());blocks.push_back(assignBlock_19());
+    if(player == 'A')
+    {
+        for(int i=0;i<shapesA.size();i++)
+        {
+            cout<<i<<":"<<endl;
+            printShape(shapesA[i]);
+        }
+    }
+    else
+    {
+        for(int i=0;i<shapesB.size();i++)
+        {
+            cout<<i<<":"<<endl;
+            printShape(shapesB[i]);
+        }
+    }
+}
+void printMap()
+{
+    cout<<"            1111"<<endl;
+    cout<<"  ";
+    for(int i=0;i<14;i++)
+    {
+        cout<<i%10;
+    }
 
+    cout<<endl;
 
+    for(int i=0;i<14;i++)
+    {
+        if(i>=10)
+            cout<<1;
+        else
+            cout<<" ";
+        cout<<i%10;
+        for(int j=0;j<14;j++)
+        {
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+bool isConnectedToShoulder(shape shp, int x, int y, char player)  // connected to a shoulder
+{
+    for(int i=0;i<5;i++)//each block of the shape
+    {                //     block(x)                              block(y)                              board(x,y)
+        for(int j=0;j<4;j++)//each shoulder of the block
+        {
+            if(board[shp.x[i]+x+shoulder[j][0]][shp.y[i]+y+shoulder[j][1]] == player)
+                return true;
+        }
+    }
+    cout<<"Unconneted!"<<endl;
+    return false;
+}
+
+bool isTouchedBySelf(shape shp, int x, int y, char player)  // touched by itself
+{
+    for(int i=0;i<5;i++)//each block of the beside
+    {                //     block(x)                              block(y)                              board(x,y)
+        for(int j=0;j<4;j++)//each side of the block
+        {
+            if(board[shp.x[i]+x+side[j][0]][shp.y[i]+y+side[j][1]] == player)
+            {
+                cout<<"Touch!"<<endl;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool isSpare(shape shp, int x, int y, char player)
+{
+    for(int i=0;i<5;i++)
+    {
+        if((shp.x[i]+x < 14 && shp.x[i]+x >= 0 && shp.y[i]+y <14 && shp.y[i]+y >= 0  && board[shp.x[i]+x][shp.y[i]+y] == '.')==false)// the block is in board range and unoccupied
+            return false;
+    }
+    return true;
+}
+bool isLegalMove(shape shp, int x, int y, char player)
+{
+    if(isConnectedToShoulder(shp, x, y, player)==false || isTouchedBySelf(shp, x, y, player)==true|| isSpare(shp, x, y, player)==false )
+        return false;
+    return true;
+}
+bool playerMove(shape shp, int shapeID, char player)
+{
+//    string sX, sY;
+    int x,y;
+    char junk, cX, cY;
+    printMap();
+    cout<<player<<"'s turn."<<endl;
+    cout<<"input x, y:";
+    cin>>x>>y;
+//    cin>>sX>>sY;
+//    int x,y;
+//    cX = sX[0]; cY = sY[0];
+//    if(cX>='0' && cX<='13')
+//    {
+//        x = cX - '0';
+//    }
+//    else if(cX>='A' && cX <='D')
+//    {
+//        x = cX - 'A'+10;
+//    }
+//    else
+//    {
+//        cout<<"Illegal move!"<<endl;
+//        return false;
+//    }
+//
+//    if(cY>='0' && cY<='13')
+//    {
+//        y = cY - '0';
+//    }
+//    else if(cY>='A' && cY <='D')
+//    {
+//        y = cY - 'A'+10;
+//    }
+//    else
+//    {
+//        cout<<"Illegal move!"<<endl;
+//        return false;
+//    }
+//
+
+    if(isLegalMove(shp, x, y, player))
+    {
+        for(int i=0;i<5;i++)
+        {
+            board[shp.x[i]+x][shp.y[i]+y] = player;
+        }
+        system("cls");
+        printMap();
+        return true;
+    }
+    else
+    {
+        cout<<"Illegal move!"<<endl;
+        return false;
+    }
 
 }
 
-int main()
-{
-    bool flag = false;
-    init();
-    string cmd="E";
-    int shape;
-    char junk;
-    block selected = blocks[0];
 
-    while(cmd!="EXIT")
+void init()
+{
+    for(int i=0;i<14;i++)
+    {
+        for(int j=0;j<14;j++)
+            board[i][j] = '.';
+    }
+    board[4][4] = 'A';
+    board[9][9] = 'B';
+    shapes.push_back(assignshape_00());shapes.push_back(assignshape_01());shapes.push_back(assignshape_02());shapes.push_back(assignshape_03());shapes.push_back(assignshape_04());
+    shapes.push_back(assignshape_05());shapes.push_back(assignshape_06());shapes.push_back(assignshape_07());shapes.push_back(assignshape_08());shapes.push_back(assignshape_09());
+    shapes.push_back(assignshape_10());shapes.push_back(assignshape_11());shapes.push_back(assignshape_12());shapes.push_back(assignshape_13());shapes.push_back(assignshape_14());
+    shapes.push_back(assignshape_15());shapes.push_back(assignshape_16());shapes.push_back(assignshape_17());shapes.push_back(assignshape_18());shapes.push_back(assignshape_19());
+    shapes.push_back(assignshape_20());
+    shapesA = shapesB = shapes;
+}
+
+void command()
+{
+
+    string cmd="E";
+    int shapeID, x, y;
+    char junk, player;
+    shape selected = shapes[0];
+    bool legalMove = false;
+    bool turn = true, isSelect = false;
+    while(true)
     {
         system("cls");
-       // cout<<char('0'-1);
-        cout<<"Select, FlipX, FlipY, TurnClockwise, TurnCounterClockwise, Print, Normalize, EXIT :";
+        player = (turn)?'A':'B';
+        // cout<<char('0'-1);
+        cout<<player<<"'s turn."<<endl;
+        cout<<"Select, Flip, Turn_clockwise, player_Move, Print_map, List_shapes, EXIT :";
         cin>>cmd;
+        if(cmd=="EXIT")
+            break;
         if(cmd == "S")
         {
-            cout<<"Block index:";
-            cin>>shape;
-            selected = blocks[shape];
+            isSelect = true;
+            cout<<"shape index:";
+            cin>>shapeID;
+            if(player == 'A')
+                selected = shapesA[shapeID];
+            else
+                selected = shapesB[shapeID];
             printShape(selected);
         }
-        else if(cmd == "FX")
-        {
-            selected = flipX(selected);
-            printShape(selected);
-        }
-        else if(cmd == "P")
-        {
-            printShape(selected);
-        }
-        else if(cmd == "TC")
+        else if(cmd == "T")
         {
             selected = turnClockwise(selected);
             printShape(selected);
         }
+        else if(cmd == "F")
+        {
+            selected = flip(selected);
+            printShape(selected);
+        }
+        else if(cmd == "M")
+        {
+            if(isSelect == false)//Hasn't selet yet
+            {
+                cout<<"Please Select a shape."<<endl;
+                cout<<"shape index:";
+                cin>>shapeID;
+                if(player == 'A')
+                    selected = shapesA[shapeID];
+                else
+                    selected = shapesB[shapeID];
+
+            }
+            printShape(selected);
+
+            legalMove = playerMove(selected, shapeID, player);
+            if(legalMove == true)
+            {
+                isSelect = false;
+                turn = !turn;
+            }
+
+        }
+        else if(cmd == "P")
+        {
+            printMap();
+        }
+        else if(cmd == "L")
+        {
+                listShapes(player);
+        }
+        else
+        {
+            cout<<"Not a command."<<endl;
+        }
+        cout<<"Input a char to continue:";
         cin>>junk;
-
-
     }
 
+}
+int main()
+{
+    bool flag = false;
+    init();
+    command();
     return 0;
 }
+
+
+
--- BlokusMar/shapes.h
+++ BlokusMar/shapes.h
@@ -3,71 +3,72 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
-int blockChoises[21];
-struct block
+int shapeChoises[21];
+struct shape
 {
     int size=0;
     int x[5]={0};
     int y[5]={0};
-	block set(string xStr, string yStr)
+    //the point to identify
+    int uniX = 0;
+    int uniY = 0;
+	void set(string xStr, string yStr)
 	{
 
 		for(int i=0;i<size;i++)
 		{
-		    cout<<"Set:";
 			x[i] = xStr[i]-'0';
 			y[i] = yStr[i]-'0';
-			cout<<xStr[i]<<", "<<yStr[i]<<endl;
 		}
 	}
 };
-block assignBlock_00()
+shape assignshape_00()
 {
     /*
     O
     */
-	block tmp;
+	shape tmp;
 	tmp.size = 1;
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_01()
+shape assignshape_01()
 {
     /*
     OO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 2;
 	tmp.set("01", "00");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_02()
+shape assignshape_02()
 {
 	/*
 	O
 	O
 	O
 	*/
-    block tmp;
+    shape tmp;
 	tmp.size = 3;
 	tmp.set("012", "000");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_03()
+shape assignshape_03()
 {
     /*
     O
     OO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 3;
 	tmp.set("011", "001");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_04()
+shape assignshape_04()
 {
     /*
     O
@@ -75,62 +76,62 @@ block assignBlock_04()
     O
     O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 4;
 	tmp.set("0123", "0000");
-   // blocks.push_back(tmp);
+   // shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_05()
+shape assignshape_05()
 {
     /*
     OOO
      O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 4;
 	tmp.set("0001", "0121");
-   // blocks.push_back(tmp);
+   // shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_06()
+shape assignshape_06()
 {
     /*
     OO
      OO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 4;
 	tmp.set("0011", "0112");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_07()
+shape assignshape_07()
 {
     /*
     OO
     OO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 4;
 	tmp.set("0011", "0101");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_08()
+shape assignshape_08()
 {
     /*
     O
     O
     OO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 4;
 	tmp.set("0122", "0001");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_09()
+shape assignshape_09()
 {
     /*
     O
@@ -139,13 +140,13 @@ block assignBlock_09()
     O
     O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("01234", "00000");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_10()
+shape assignshape_10()
 {
     /*
     O
@@ -153,225 +154,223 @@ block assignBlock_10()
     O
     OO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("01233", "00001");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_11()
+shape assignshape_11()
 {
     /*
     O O
     OOO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("01110", "00122");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_12()
+shape assignshape_12()
 {
     /*
     OO
      O
      OO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("00122", "01112");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_13()
+shape assignshape_13()
 {
     /*
     OOO
      O
      O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("00012", "01211");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_14()
+shape assignshape_14()
 {
     /*
      O
     OOO
      O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("000/1", "01211");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_15()
+shape assignshape_15()
 {
     /*
     OO
      OO
       O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("00112", "01122");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_16()
+shape assignshape_16()
 {
     /*
     O
     O
     OOO
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("01222", "00012");
-   // blocks.push_back(tmp);
+   // shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_17()
+shape assignshape_17()
 {
     /*
     O
     OOO
      O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("01112", "00121");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_18()
+shape assignshape_18()
 {
     /*
     OO
     OO
     O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("00112", "01010");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_19()
+shape assignshape_19()
 {
     /*
     OOOO
      O
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("00001", "01231");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
-block assignBlock_20()
+shape assignshape_20()
 {
     /*
     OO
      OOo
     */
-    block tmp;
+    shape tmp;
 	tmp.size = 5;
 	tmp.set("00111", "01123");
-    //blocks.push_back(tmp);
+    //shapes.push_back(tmp);
     return tmp;
 }
 
 
-bool isBlock(block blk, int x, int y)
+bool isshape(shape shp, int x, int y)
 {
-    for(int i=0;i<blk.size;i++)
+    for(int i=0;i<shp.size;i++)
     {
-        if(x==blk.x[i] && y == blk.y[i])
+        if(x==shp.x[i] && y == shp.y[i])
             return true;
     }
     return false;
 }
 
-block flipX(block blk)
+shape flip(shape shp)
 {
     int minX = 5;
     for(int i=0; i<5; i++)    //flip
     {
-        blk.x[i] = -1 * blk.x[i];
-        minX = (blk.x[i] < minX)? blk.x[i] : minX;
+        shp.x[i] = -1 * shp.x[i];
+        minX = (shp.x[i] < minX)? shp.x[i] : minX;
     }
-    return blk;
+    return shp;
 }
 
-block flipY(block blk)
-{
-
-    for(int i=0; i<5; i++)    //flip
-    {
-        blk.y[i] = -1 * blk.y[i];
-    }
-    return blk;
-}
-block turnClockwise(block blk)
+shape turnClockwise(shape shp)
 {
     int tmp[5] = {0};
     for(int i=0; i<5; i++)
     {
-        tmp[i] = blk.x[i];
-        blk.x[i] = blk.y[i];
-        blk.y[i] = -1 * tmp[i];
+        tmp[i] = shp.x[i];
+        shp.x[i] = shp.y[i];
+        shp.y[i] = -1 * tmp[i];
     }
-    return blk;
+    return shp;
 }
 
-block turnCounter_Clockwise(block blk)
+shape turnCounter_Clockwise(shape shp)
 {
     int tmp[5] = {0};
     for(int i=0; i<5; i++)
     {
-        tmp[i] = blk.x[i];
-        blk.x[i] = -1 * blk.y[i];
-        blk.y[i] = tmp[i];
+        tmp[i] = shp.x[i];
+        shp.x[i] = -1 * shp.y[i];
+        shp.y[i] = tmp[i];
     }
-    return blk;
+    return shp;
 }
 
-block normalizeBlock(block blk)
+shape normalizeshape(shape shp)
 {
     int minX = 0, minY = 0;
     for(int i=0; i<5; i++)
     {
-        minX = (blk.x[i] < minX) ? blk.x[i] : minX;
-        minY = (blk.y[i] < minY) ? blk.y[i] : minY;
+        minX = (shp.x[i] < minX) ? shp.x[i] : minX;
+        minY = (shp.y[i] < minY) ? shp.y[i] : minY;
     }
     for(int i=0; i<5; i++)
     {
-        blk.x[i] -= minX;
-        blk.y[i] -= minY;
+        shp.x[i] -= minX;
+        shp.y[i] -= minY;
     }
-    return blk;
+    shp.uniX -= minX; shp.uniY -= minY;
+    return shp;
 }
 
 
-void printShape(block blk)
+void printShape(shape shp)
 {
-    blk = normalizeBlock(blk);
+    shp = normalizeshape(shp);
     for(int i=0;i<5;i++)
     {
         for(int j=0;j<5;j++)
         {
-            if(isBlock(blk, i, j))
-                cout<<"*";
-            else cout<<"0";
+            if(isshape(shp, i, j))
+            {
+                if(i==shp.uniX && j==shp.uniY)
+                    cout<<"*";
+                else
+                    cout<<"0";
+            }
+            else cout<<" ";
         }
         cout<<endl;
     }
 }
+
 
 
